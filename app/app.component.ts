b5acd4cd74739f8be7e5bc8864083269b4801949
sagittaria <shuyang_x@hotmail.com>
2016-10-02 19:13:07 +0800

    把英雄详情页（模板）剪出来

--- app/app.component.ts
+++ app/app.component.ts
@@ -29,15 +29,7 @@ const HEROES: Hero[] = [
             <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
               <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
-           </ul>
-           <div *ngIf="selectedHero">
-             <h2>{{selectedHero.name}} details!</h2>
-             <div><label>id: </label>{{selectedHero.id}}</div>
-             <div>
-                <label>name: </label>
-                 <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-             </div>
-            </div> 
+           </ul> 
            `,
   styles: [`
           .selected {

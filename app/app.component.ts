d5b98e127cf888de2ff72952f0da4f844af4fb3a
sagittaria <shuyang_x@hotmail.com>
2016-10-02 18:57:13 +0800

    加“选中样式”

--- app/app.component.ts
+++ app/app.component.ts
@@ -23,9 +23,10 @@ const HEROES: Hero[] = [
 @Component({
   selector: 'my-app',
   template:`
+           <h1>{{title}}</h1>
            <h2>My Heroes</h2>
            <ul class="heroes">
-            <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+            <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
               <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
            </ul>

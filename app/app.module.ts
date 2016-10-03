3f62b579f7db375a3211c70a5e397ed1e4cbde98
kasoya <shuyang_x@hotmail.com>
2016-10-03 18:17:37 +0800

    一些东西被提到模块里（没明白）

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,9 +4,12 @@ import { FormsModule }    from '@angular/forms';
 
 import { AppComponent }   from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+  providers: [HeroService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
